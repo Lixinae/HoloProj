675bf4506159ec78d4dcca01f83583569e64f86a
Vahen <Vahen@users.noreply.github.com>
2018-07-04 13:10:04 +0200

    Ajout du debug helper

---
+++ Assets/Scripts/DebugHelper.cs
@@ -0,0 +1,48 @@
+﻿using HoloToolkit.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugHelper : Singleton<DebugHelper> {
+
+    public Text debugText = null;
+
+    private Dictionary<int,string> dictionary;
+    private List<string> debugTexts;
+    // Use this for initialization
+    void Start() {
+        if (debugText == null) {
+            debugText = GameObject.Find("Text").GetComponent<Text>();
+        }
+    }
+
+    private void ResetText() {
+        debugText.text = "";
+    }
+
+    public void AddDebugText(string textToAdd,int id) {
+        if (dictionary.ContainsKey(id)) {
+            dictionary[id] = textToAdd;
+        }
+        else {
+            dictionary.Add(id, textToAdd);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        string output = ConcatDico();
+        debugText.text = output;
+    }
+
+    private string ConcatDico() {
+        string output = "";
+        foreach (KeyValuePair<int, string> entry in dictionary) {
+            output += entry.Value;
+        }
+        return output;
+    }
+}
