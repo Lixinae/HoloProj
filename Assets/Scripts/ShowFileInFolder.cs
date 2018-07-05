6789aefdac6fd216cc818b23f2cde99534f483ad
Vahen <Vahen@users.noreply.github.com>
2018-07-05 16:44:00 +0200

    Ajout de la copie de fichier dans un repertoire utilisable en ecriture

--- Assets/Scripts/ShowFileInFolder.cs
+++ Assets/Scripts/ShowFileInFolder.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using UnityEditor;
+
 
 
 #if !UNITY_EDITOR
 using Windows.Storage;
 using Windows.Storage.Search;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
 #endif
 public class ShowFileInFolder {
 
     private List<String> fileGLtfList;
+    private bool copyOnce = false;
 
     public ShowFileInFolder() {
         fileGLtfList = new List<string>();
+    }
+#if !UNITY_EDITOR
+    // Permet de copier les fichiers
+    private async Task CopyFolderAsync(StorageFolder source, StorageFolder destinationContainer, string desiredName = null) {
+        StorageFolder destinationFolder = null;
+        destinationFolder = await destinationContainer.CreateFolderAsync(
+            desiredName ?? source.Name, CreationCollisionOption.ReplaceExisting);
+
+        foreach (var file in await source.GetFilesAsync()) {
+            await file.CopyAsync(destinationFolder, file.Name, NameCollisionOption.ReplaceExisting);
+        }
+        foreach (var folder in await source.GetFoldersAsync()) {
+            await CopyFolderAsync(folder, destinationFolder);
+        }
+    }
+#endif
+    // Permet de copier un dossier source dans un dossier destination en gardant l'arborescence
+    // D:/bidule/chose -> E:/a/b/destination/bidule/chose
+    // Recursivement
+    private void CopyFolder(String source,String destination) {
+        Directory.CreateDirectory(destination + Path.GetDirectoryName(source));
+
+        foreach(var file in Directory.GetFiles(source)) {
+            File.Copy(file, destination + Path.GetFileName(file));
+        }
+        foreach (var folder in Directory.GetDirectories(source)) {
+            CopyFolder(folder,destination);
+        }
     }
 
     private void GetAllFileNames() {
@@ -24,9 +56,48 @@ public class ShowFileInFolder {
         string[] allfiles = Directory.GetFiles(path, "*.gltf", SearchOption.AllDirectories);
         CleanAndAddToList(allfiles);
 #else
-        Task task = new Task(
+        // Copier le dossier "path" vers le current.LocalFolder
+
+        // TODO Tester
+        String path = Application.streamingAssetsPath + "/3DModels";
+
+        String test2 = ApplicationData.Current.LocalFolder.Path;
+        if (!copyOnce) {
+            Debug.Log("Copying files");
+            CopyFolder(path, test2);
+            copyOnce = true;
+        }
+        Debug.Log("test bidule chose2 : " + test2);
+        string[] allfilesTest = Directory.GetFiles(test2, "*.*", SearchOption.AllDirectories);
+
+        string[] allfilesTest2 = Directory.GetFiles(test2, "*.gltf", SearchOption.AllDirectories);
+        foreach(string s in allfilesTest) {
+            Debug.Log("String :" + s);
+        }
+
+        foreach (string s in allfilesTest2) {
+            Debug.Log("String gltf only:" + s);
+        }
+        CleanAndAddToList(allfilesTest2);
+        
+
+        // Voir si pas moyen de repliquer le code au dessus plutot que l'espèce de purge en dessous
+
+        /*Task task = new Task(
             async () => {
                 StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                // On ne copie qu'une seul fois
+                if (!copyOnce) {
+                    StorageFolder models = await Package.Current.InstalledLocation.GetFolderAsync("StreamingAssets\\3DModels");
+                    Debug.Log("Models folder: " + models.DisplayName);
+                    DebugHelper.Instance.AddDebugText("Models folder: " + models.DisplayName, 4);
+                    await CopyFolderAsync(models, localFolder);
+                    copyOnce = true;
+                }
+
+
+                DebugHelper.Instance.AddDebugText("LocalFolder : " + localFolder.DisplayName, 3);
+                Debug.Log("LocalFolder : " + localFolder.DisplayName);
                 StorageFolderQueryResult queryResultFolder3D = localFolder.CreateFolderQuery(CommonFolderQuery.DefaultQuery);
                 IReadOnlyList<StorageFolder> folderList = await queryResultFolder3D.GetFoldersAsync();
                 // On recupère la liste des dossier de local folder
@@ -47,10 +118,9 @@ public class ShowFileInFolder {
                                 // Print the name of the file.
                                 if (file.Name.EndsWith("*.gltf")) {
                                     string fileName = localFolder.Name + "/" + folder3D.Name + "/" + folder.Name + "/" + file.Name;
+                                    Debug.Log("FileName : " + fileName);
                                     DebugHelper.Instance.AddDebugText("FileName : "+ fileName, 1);
                                     allfiles.Add(fileName);
-
-
                                 }
                             }
                         }
@@ -62,7 +132,7 @@ public class ShowFileInFolder {
                 CleanAndAddToList(allfiles.ToArray());
             });
         task.Start();
-        task.Wait();
+        task.Wait();*/
 
 #endif
     }
