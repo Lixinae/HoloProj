81733c7be869755765a7b40ce52eafb1d142e3bb
Vahen <Vahen@users.noreply.github.com>
2018-07-24 16:32:54 +0200

    Correctifs

--- Assets/Scripts/UI/UITriggers.cs
+++ Assets/Scripts/UI/UITriggers.cs
@@ -1,9 +1,4 @@
-﻿using HoloToolkit.Examples.InteractiveElements;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UITriggers : MonoBehaviour {
 
