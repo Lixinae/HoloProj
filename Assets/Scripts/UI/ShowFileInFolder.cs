ee1c9090689c8f33447f2779ff4d0d5f720245ed
picoc <picoc   @pc5206d.lan.esiee.fr>
2018-07-26 16:14:05 +0200

    Revert "Correctifs - suppression de code inutile, suppression de fichier inutile"
    This reverts commit 095c6ac3ab87deac4004bd855f61eb9575af432c.

--- Assets/Scripts/UI/ShowFileInFolder.cs
+++ Assets/Scripts/UI/ShowFileInFolder.cs
@@ -14,7 +14,7 @@ using Windows.ApplicationModel;
 public class ShowFileInFolder {
 
     private List<String> fileGLtfList;
-    //private bool copyOnce = false;
+    private bool copyOnce = false;
 
     public ShowFileInFolder() {
         fileGLtfList = new List<string>();
@@ -49,13 +49,10 @@ public class ShowFileInFolder {
     }
 
     private void GetAllFileNames() {
-        // Remettre UNITY_EDITOR ici une fois corrigé
+#if UNITY_EDITOR
         String path = Application.streamingAssetsPath + "/3DModels";
         string[] allfiles = Directory.GetFiles(path, "*.gltf", SearchOption.AllDirectories);
         CleanAndAddToList(allfiles);
-
-#if UNITY_EDITOR
-
 #else
         // Copier le dossier "path" vers le current.LocalFolder
 
@@ -88,11 +85,7 @@ public class ShowFileInFolder {
         // Voir si pas moyen de repliquer le code au dessus plutot que l'espèce de purge en dessous
 
         // todo -> erreurs pour le moment
-
-        ///
-        /// 
-        /// TODO enlever le commentaire 
-        /*Task task = new Task(
+        Task task = new Task(
             () => {
                 String path = Application.streamingAssetsPath + "\\3DModels";
                 Debug.Log("Started async task");
@@ -126,52 +119,45 @@ public class ShowFileInFolder {
                 CleanAndAddToList(allfilesTest2);
                 Debug.Log("Finished async task");
                 DebugHelper.Instance.AddDebugText("Finished async task", 5);
-                */
+                /*
+                DebugHelper.Instance.AddDebugText("LocalFolder : " + localFolder.DisplayName, 3);
+                Debug.Log("LocalFolder : " + localFolder.DisplayName);
+                StorageFolderQueryResult queryResultFolder3D = localFolder.CreateFolderQuery(CommonFolderQuery.DefaultQuery);
+                IReadOnlyList<StorageFolder> folderList = await queryResultFolder3D.GetFoldersAsync();
+                // On recupère la liste des dossier de local folder
+                // Si un dossier porte le nom de "3DModels" on scanne son contenu et on construit la liste des fichiers
+                // 
 
+                List<string> allfiles = new List<string>();
 
-        /*
-        DebugHelper.Instance.AddDebugText("LocalFolder : " + localFolder.DisplayName, 3);
-        Debug.Log("LocalFolder : " + localFolder.DisplayName);
-        StorageFolderQueryResult queryResultFolder3D = localFolder.CreateFolderQuery(CommonFolderQuery.DefaultQuery);
-        IReadOnlyList<StorageFolder> folderList = await queryResultFolder3D.GetFoldersAsync();
-        // On recupère la liste des dossier de local folder
-        // Si un dossier porte le nom de "3DModels" on scanne son contenu et on construit la liste des fichiers
-        // 
-
-        List<string> allfiles = new List<string>();
-
-        // Correspond a string[] allfiles = Directory.GetFiles(path, "*.gltf", SearchOption.AllDirectories); dans unity
-        foreach (StorageFolder folder3D in folderList) {
-            DebugHelper.Instance.AddDebugText("No unity editor Path = " + folder3D.DisplayName, 1);
-            if (folder3D.DisplayName == "3DModels") {
-                StorageFolderQueryResult queryResult = folder3D.CreateFolderQuery(CommonFolderQuery.DefaultQuery);
-                IReadOnlyList<StorageFolder> subfolderList = await queryResult.GetFoldersAsync();
-                foreach (StorageFolder folder in subfolderList) {
-                    IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
-                    foreach (StorageFile file in fileList) {
-                        // Print the name of the file.
-                        if (file.Name.EndsWith("*.gltf")) {
-                            string fileName = localFolder.Name + "/" + folder3D.Name + "/" + folder.Name + "/" + file.Name;
-                            Debug.Log("FileName : " + fileName);
-                            DebugHelper.Instance.AddDebugText("FileName : "+ fileName, 1);
-                            allfiles.Add(fileName);
+                // Correspond a string[] allfiles = Directory.GetFiles(path, "*.gltf", SearchOption.AllDirectories); dans unity
+                foreach (StorageFolder folder3D in folderList) {
+                    DebugHelper.Instance.AddDebugText("No unity editor Path = " + folder3D.DisplayName, 1);
+                    if (folder3D.DisplayName == "3DModels") {
+                        StorageFolderQueryResult queryResult = folder3D.CreateFolderQuery(CommonFolderQuery.DefaultQuery);
+                        IReadOnlyList<StorageFolder> subfolderList = await queryResult.GetFoldersAsync();
+                        foreach (StorageFolder folder in subfolderList) {
+                            IReadOnlyList<StorageFile> fileList = await folder.GetFilesAsync();
+                            foreach (StorageFile file in fileList) {
+                                // Print the name of the file.
+                                if (file.Name.EndsWith("*.gltf")) {
+                                    string fileName = localFolder.Name + "/" + folder3D.Name + "/" + folder.Name + "/" + file.Name;
+                                    Debug.Log("FileName : " + fileName);
+                                    DebugHelper.Instance.AddDebugText("FileName : "+ fileName, 1);
+                                    allfiles.Add(fileName);
+                                }
+                            }
                         }
                     }
+                    else {
+                        continue;
+                    }
                 }
-            }
-            else {
-                continue;
-            }
-        }
-        CleanAndAddToList(allfiles.ToArray());*/
-        ///
-        /// 
-        /// TODO enlever le commentaire 
-        //}); 
+                CleanAndAddToList(allfiles.ToArray());*/
+            });
         //task.Start();
         //task.Wait();
-
-        //task.RunSynchronously();
+        task.RunSynchronously();
 
 #endif
     }
